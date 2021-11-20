53a80ab952030ccca087ccbd94c8dddfb9f3a696
Robert McMonagle <robert@solutionjar.com>
2021-11-19 22:45:18 -0800

    Created backend folder

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import Koa from 'koa';
+var serve = require('koa-static');
+const mount = require('koa-mount');
+import { ApolloServer } from 'apollo-server-koa';
+
+const main = async () => {
+
+}
+
+main();
